a9237bf93682f81f72536ecbe21686f0a8301cbc
jinweicheng <cjw1312940166@163.com>
2022-06-24 10:53:33 +0800

    个人中心init

---
+++ src/views/layout/index.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <div class="layout-container">
+        layout
+    </div>
+</template>
+<script>
+export default {
+    name: 'layout',
+    components: {},
+    props: {},
+}
+</script>
+<style lang="">
+    
+</style>
